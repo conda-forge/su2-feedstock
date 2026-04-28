57de9eb9b832544b10eeab8ec3beff517390eb5a
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-04-28 09:31:02 +0300

    Set -march=armv8.4-a for cross buiding for aarch64
    ```
     │ │ cc1plus: error: unknown value 'native' for '-march'
     │ │ Assembler messages:
     │ │ Error: unknown architecture `native'
     │ │ Error: unrecognized option `-march=native'
     │ │ cc1plus: note: valid arguments are: armv8-a armv8.1-a armv8.2-a armv8.3-a armv8.4-a armv8.5-a armv8.6-a armv8.7-a armv8.8-a armv8.9-a armv8-r armv9-a armv9.1-a armv9.2-a armv9.3-a armv9.4-a
     ```

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,11 @@
 set -xe
 
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+MARCH="native"
+if test ${HOST} == "aarch64-conda-linux-gnu"; then
+  MARCH="armv8.4-a"
+fi
 
-./meson.py build --prefix=${PREFIX} -Denable-openblas=true -Denable-pywrapper=true -Denable-autodiff=true -Denable-directdiff=true
+./meson.py build --prefix=${PREFIX} -Dcpu-arch=${MARCH} -Denable-openblas=true -Denable-pywrapper=true -Denable-autodiff=true -Denable-directdiff=true
 
 ./ninja -C build install
